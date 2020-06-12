641cf701ab1c287e211a24b89ee288d4e291be84
Rick Dale <racdale@gmail.com>
2020-06-12 13:20:52 -0700

    Update combineMansonData.R

--- combineMansonData.R
+++ combineMansonData.R
@@ -1,8 +1,11 @@
-#mansonDataAll = read.xlsx('Manson_Convo_PD_data.xlsx',sheetIndex=1,startRow=2)
+# first: set working directory to here
 mansonDataAll = read.csv('Manson_Convo_PD_data.csv',header=T,skip=1)
 colnames(mansonDataAll) = list('order','triad','code','chairs','p1.id','sex','p1.income.zip','p1.primary.psycho','p1.secondary.psycho','p2.facial','cultural.style','language.style.match','common.ground','p2.interrupts.p1','p1.pd.toward.p2','p2.pd.toward.p1','p1.rates.p2.warmth','p1.rates.p2.competence','p2.rates.p1.warmth','p2.rates.p1.competence')
 
-mansonData = mansonDataAll #[mansonDataAll$chairs=='LC'|mansonDataAll$chairs=='CR'|mansonDataAll$chairs=='LR',] # in case we want to focus on one chair ordering
+mansonData = mansonDataAll 
+# in case we want to focus analysis on one chair ordering
+# mansonData = mansonDataAll[mansonDataAll$chairs=='LC'|mansonDataAll$chairs=='CR'|mansonDataAll$chairs=='LR',] 
+
 laughs = read.table('laughter_data.txt',header=T,sep='\t') # laughter data from bryant, several available columns, pre-selected 2 obvious ones
 
 mansonData$winmin = -1 # initialize the columns we will populate with the body DVs
@@ -26,11 +29,12 @@ for (i in 1:dim(wccresNew)[1]) { # integrating Manson data with body correlation
   triad = as.numeric(substr(wccresNew[i,]$triad,2,ix)) # get triad # from body movement file, e.g., T8_...
   mansonLaughs = rbind(mansonLaughs,laughs[laughs$CONV==triad,2:3],laughs[laughs$CONV==triad,2:3]) # get the laughs
   # get measures from both orderings in manson data (LR / RL)
-  mansonData[mansonData$chairs==wccres[i,]$typ & mansonData$triad==triad,21:25] = wccresNew[i,4:8]
-  mansonData[mansonData$chairs==reverseString(wccres[i,]$typ) & mansonData$triad==triad,21:25] = wccresNew[i,4:8]
+  mansonData[mansonData$chairs==wccresNew[i,]$typ & mansonData$triad==triad,21:25] = wccresNew[i,4:8]
+  mansonData[mansonData$chairs==reverseString(wccresNew[i,]$typ) & mansonData$triad==triad,21:25] = wccresNew[i,4:8]
 }
 
-# let's retrieve the windowed correlation scores and treat this as a repeatedmeasures setup... multiple observations in 10s segments
+# let's retrieve the windowed correlation scores and treat this as a repeated
+# measures setup... multiple observations in 10s segments
 # increases power for the exploratory analysis as described in the main paper
 wccMansonData = wccfull[wccfull$cond=='obs',]
 desiredCols = c('p1.id','sex','p1.income.zip','p1.primary.psycho','p2.facial',
@@ -45,7 +49,8 @@ lapply(desiredCols,function(x) {
 l = nrow(wccMansonData)
 wccMansonDataRev = wccMansonData # so we can get the reverse-chair scores
 laughDat = c()
-# a slow and lame loop, I know... but we just do it once and we're done
+# a slow and lame loop... but we just do it once and we're done
+# *NB: wccMansonData = windowed correlations combined with Manson et al. covariates
 for (i in 1:l) {
   print(i/l)
   ix=regexpr('_',wccMansonData[i,]$triad)[1]-1
@@ -56,11 +61,8 @@ for (i in 1:l) {
   typ = wccMansonData[i,]$typ
   revTyp = reverseString(wccMansonData[i,]$typ)
   mansonVect = mansonData[mansonData$chairs==typ&mansonData$triad==triad,]
-  wccMansonData[i,6:17] = subset(mansonVect,select=desiredCols)
+  wccMansonData[i,7:18] = subset(mansonVect,select=desiredCols)
   mansonVect = mansonData[mansonData$chairs==revTyp&mansonData$triad==triad,]
-  wccMansonData[i,6:17] = (wccMansonData[i,6:17] + subset(mansonVect,select=desiredCols))/2
+  wccMansonData[i,7:18] = (wccMansonData[i,7:18] + subset(mansonVect,select=desiredCols))/2
 }
-
-
-
 
